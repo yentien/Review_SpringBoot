47685bf7a56ce41a0cfa7a1fa18fd7b8a0dcd3af
Arthur Tien <arhturtien88@gmail.com>
2023-03-19 21:09:45 +0800

    Initial commit

---
+++ src/main/java/com/arthur/review_springboot/ReviewSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.arthur.review_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReviewSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReviewSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/arthur/review_springboot/ReviewSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arthur.review_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReviewSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
